8bec91a4770136c17151f11cdb1326cdb07bd808
muskan shukla <muskanshukla707@gmail.com>
2024-06-25 19:23:02 +0530

    Reccursion code added

---
+++ Recursion/FactOfNum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FactOfNum {
+
+    public static int factNum(int n){
+
+        int result;
+
+        if(n==0 || n==1){ //BASE CASE CONDITION
+            result =1;
+
+        }
+        else{
+
+            result= n*factNum(n-1); //RECURSIVE CALL
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter any number to find factorial");
+        int n=sc.nextInt();
+
+        int result= factNum(n);
+
+        System.out.println("Factorial of the given number is: "+ result);
+    }
+}
